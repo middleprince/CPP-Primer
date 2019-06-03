e785f576e6b84f5f6d79297f92441be5679d6662
middleprince <hellangel.ah+github@gmail.com>
2019-06-03 17:24:56 +0800

    feat: Add exercise about static class member

---
+++ Chapter-7-Classes/ex7.58-static.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+using std::vector;
+
+class Example {
+public:     
+    static constexpr double rate = 6.5;
+    //  static member must be initialize out of the class, or 
+    //  define as static constexpr double rate = 6.5;
+    static const int vecSize = 20;
+    
+    static vector<double> vec;
+    // non-static members can not be used as default arguments.
+
+};
+
+// declareation of static member must be out of function
+vector<double> Example::vec;
+const double Example::rate;
+
+int main() {
+    return 0;
+}
