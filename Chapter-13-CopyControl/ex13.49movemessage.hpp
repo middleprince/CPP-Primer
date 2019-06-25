c57173e1fcc6d7441c30fec7d1762f64b1349660
middleprince <hellangel.ah@gmail.com>
2019-06-25 10:44:58 +0800

    feat: Add move constructor and move assinment constructor

---
+++ Chapter-13-CopyControl/ex13.49movemessage.hpp
@@ -0,0 +1,229 @@
+//
+//  ex13_4meesagefolder.h
+//  Exercise 13.34 13.36 13.37
+//
+//   34: Write the Message class as described in this section.
+//
+//   36: Design and implement the corresponding Folder class. That class should
+//   hold a set that points to the Messages in that Folder.
+//
+//   37:  Add members to the Message class to insert or remove a given Folder*
+//   into folders.
+//        These members are analogous to Folder’s addMsg and remMsg operations.
+#ifndef ex13_19movemessage_h
+#define ex13_19movemessage_h
+
+#include <string>
+#include <set>
+#include <iostream>
+
+using std::string;
+using std::set;
+using std::cout;
+using std::endl;
+
+class Folder;
+
+class Message {
+    friend class Folder;
+    friend void swap(Message&, Message&);
+    friend void swap(Folder&, Folder&);
+    
+public:
+    explicit Message(const string &strs = string()) 
+        : contents(strs) {}
+    Message(const Message &);
+    Message & operator=(const Message &);
+
+     ~Message();
+    
+    // move constructor
+    Message(Message &&);
+    Message & operator=(Message &&);
+    
+
+     void save(Folder &);
+     void remove(Folder &);
+
+     void printMsg();
+    
+private:
+    string contents;
+    set<Folder*> folders;
+    
+    void add_to_Folder(const Message&);
+    void remove_from_Folder();
+
+    // tool function for move constructor and assigne constructor
+    void move_Folders(Message *m);
+
+};
+
+void swap(Message&, Message&);
+
+class Folder {
+    friend void swap(Message&, Message&);
+    friend void swap(Folder&, Folder&);
+    friend class Message;
+
+public:
+    Folder() = default;
+    Folder(const Folder&);
+    Folder & operator=(const Folder&);
+    ~Folder();
+
+    void printMsg();
+
+private:
+    set<Message*> msgs;
+
+    void add_to_Messages(const Folder&);
+    void remove_from_Messages();
+
+    void addMsg(Message *m) {msgs.insert(m);}
+    void remMsg(Message *m) {msgs.erase(m);}
+
+};
+
+
+void swap(Folder&, Folder&);
+
+void swap(Message &lhs, Message &rhs) {
+    using std::swap;
+    for (auto f : lhs.folders)
+        f->remMsg(&lhs);
+    for (auto f : rhs.folders)
+        f->remMsg(&rhs);
+
+    swap(lhs.folders, rhs.folders);
+    swap(lhs.contents, rhs.contents);
+
+    for (auto f : lhs.folders)
+        f->addMsg(&lhs);
+    for (auto f : rhs.folders)
+        f->addMsg(&rhs);
+}
+
+Message::Message(const Message &old) 
+    : contents(old.contents), folders(old.folders) {
+        add_to_Folder(old); 
+    }
+
+Message & Message::operator=(const Message &rval) {
+    remove_from_Folder();
+    contents = rval.contents;
+    folders = rval.folders;
+    add_to_Folder(rval);
+    return *this;
+}
+
+Message::~Message() {
+    remove_from_Folder();
+}
+
+// move constructor
+Message::Message(Message &&old) 
+    : contents(std::move(old.contents)) {
+        move_Folders(&old);
+    }
+
+// move assignment constructor
+Message & Message::operator=(Message &&rhs) {
+    if (this != &rhs) {
+        remove_from_Folder();
+        contents = std::move(rhs.contents);
+        move_Folders(&rhs);
+    }
+    return *this;
+}
+
+// add message into the folder f
+void Message::save(Folder &f) {
+    folders.insert(&f);   
+    f.addMsg(this);
+}
+
+// remove message from folder f
+void Message::remove(Folder &f) {
+    folders.erase(&f);
+    f.remMsg(this);
+}
+
+// tool function, add message pointer to folders
+void Message::add_to_Folder(const Message &m) {
+    for (auto &f : m.folders)
+        f->addMsg(this);
+}
+
+// tool function, remove message pionter according to folders 
+void Message::remove_from_Folder() {
+    for (auto &f : folders)
+        f->remMsg(this);
+}
+
+// tool function, remove_Fodlers()
+// move the folders to new destiney
+void Message::move_Folders(Message *m) {
+    folders = std::move(m->folders);
+    for (auto f : folders) {
+        f->remMsg(m); 
+        f->addMsg(this);
+    }
+    m->folders.clear();
+    
+
+}
+
+void Message::printMsg() {
+    cout << contents << endl;
+}
+
+void swap(Folder &lhs, Folder &rhs) {
+    using std::swap;
+    lhs.remove_from_Messages();
+    rhs.remove_from_Messages();
+
+    swap(lhs.msgs, rhs.msgs);
+
+    lhs.add_to_Messages(lhs);
+    rhs.add_to_Messages(rhs);
+}
+
+Folder::Folder(const Folder &f) : msgs(f.msgs) {
+    add_to_Messages(f);
+}
+
+Folder & Folder::operator=(const Folder &rhs) {
+    remove_from_Messages();
+    msgs = rhs.msgs;
+    add_to_Messages(rhs);
+    return *this;
+}
+
+Folder::~Folder() {
+   remove_from_Messages();
+}
+
+// tool function
+// add the current Folder pointer to other Folder's message's folders 
+void Folder::add_to_Messages(const Folder &f) {
+    for (auto m : f.msgs)
+        m->folders.insert(this);
+}
+
+// tool function
+// remove Folder infon from messages
+void Folder::remove_from_Messages() {
+    for (auto m : msgs)
+        m->folders.erase(this);
+}
+
+void Folder::printMsg() {
+    for (auto m : msgs)
+        cout << m->contents << " ";
+   cout << endl; 
+}
+    
+
+
+#endif
