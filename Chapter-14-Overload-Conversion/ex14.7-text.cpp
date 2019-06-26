db263054bfac4525b0863ebdb8c439a9e18000d3
middleprince <hellangel.ah@gmail.com>
2019-06-26 21:54:23 +0800

    feat: Add few exercise in Chap14

---
+++ Chapter-14-Overload-Conversion/ex14.7-text.cpp
@@ -0,0 +1,7 @@
+#include "ex14.7.String.hpp"
+
+int main() {
+    String a("hello me and you");
+    std::cout << a << std::endl;
+    return 0;
+}
---
+++ Chapter-14-Overload-Conversion/ex14.7.String.hpp
@@ -0,0 +1,92 @@
+#ifndef ex14_7_String_h
+#define ex14_7_String_h
+
+#include <memory>
+#include <algorithm>
+#include <iostream>
+
+
+class String {
+    friend std::ostream& operator<<(std::ostream&, const String&);
+public:
+    String() : String("") {}
+    String(const char *);
+    String(const String&);
+    String & operator=(const String&);
+    ~String();
+
+    const char* c_str() const {return elements; }
+    size_t size() {return end - elements;}
+    size_t lenght() {return end - elements -1; }
+
+private:
+    char *elements;
+    char *end;
+    std::allocator<char> alloc;
+
+    std::pair<char*, char*> alloc_n_copy(const char *, const char *);
+    void free();
+    void initialize_range(const char *, const char *);
+};
+
+String::String(const char *s) {
+    char *sl = const_cast<char*>(s);
+    while(*sl) 
+        ++sl;
+    initialize_range(s, ++sl);
+    std::cout << "String() called for copy construct" << std::endl;
+}
+
+String::String(const String &old) {
+    initialize_range(old.elements, old.end);
+
+}
+
+String & String::operator=(const String &rhs) {
+    auto newdata = alloc_n_copy(rhs.elements, rhs.end);
+    free();
+    elements = newdata.first;
+    end = newdata.second; 
+    std::cout << " = copy assign constructor called" << std::endl;
+
+    return *this;
+
+}
+
+String::~String() {
+    free();
+}
+
+
+
+std::pair<char*, char*>
+String::alloc_n_copy(const char *b, const char *e) {
+    auto datanew = alloc.allocate(e - b); 
+    return {datanew, std::uninitialized_copy(b, e, datanew) };
+}
+
+void String::initialize_range(const char *first, const char *last) {
+    auto newdata = alloc_n_copy(first, last);
+    elements = newdata.first;
+    end = newdata.second;
+}
+
+void String::free() {
+   if(elements) {
+       std::for_each(elements, end, [this](char &c) { alloc.destroy(&c); }); 
+       alloc.deallocate(elements, end - elements);
+        
+   } 
+
+}
+
+// output string operator << for String
+std::ostream& operator<<(std::ostream &os, const String &item) {
+    auto bg = item.c_str();
+    while(*bg)
+        os << *bg++;
+    return os;
+}
+#endif
+
+
