8e727cee2cbbf086c6b53172411390642e7abf55
middleprince <hellangel.ah@gmail.com>
2019-07-28 09:36:47 +0800

    feat: Finish all tests

--- Chapter-16-Template/ex16.12-strBlobTest.cpp
+++ Chapter-16-Template/ex16.12-strBlobTest.cpp
@@ -1,4 +1,4 @@
-#include "ex16.21.strBlob.hpp"
+#include "ex16.12.strBlob.hpp"
 
 using std::cout;
 using std::endl;
@@ -7,18 +7,57 @@ using std::string;
 int main() {
     Blob<string> theStrings{"hello", "me", "you"};
     theStrings.push_back(std::move("king"));
+    
+    Blob<int> theInts{100, 200, 300};
 
+    // test for copy constructor
     Blob<string> theStringscp = theStrings;
 
+    // for move copy constructor
+    Blob<string> theString_moving = std::move(theStrings);
+    
+    cout << "## Test for const iterator for theInts" << endl;
+    for (auto item = theInts.cbegin(); item != theInts.cend(); ++item)
+        cout << *item << " ";
+    cout << endl;
+
+    cout << "## Test for const iterator for theString" << endl;
     for (auto item = theStrings.cbegin(); item != theStrings.cend(); ++item)
         cout << *item << " ";
     cout << endl;
     
-
+    cout << "## Test for const iterator for theStringscp" << endl;
+    theStringscp.push_back("copy");
     for (auto item = theStringscp.cbegin(); item != theStringscp.cend(); ++item)
         cout << *item << " ";
     cout << endl;
-    
+   
+    cout << "## Test for const iterator for theString_moving" << endl;
+    for (auto item = theString_moving.cbegin(); item != theString_moving.cend(); ++item)
+        cout << *item << " ";
+    cout << endl;
 
+    cout << "## Test for iterator " << endl;
+    for (auto &item : theInts) { 
+       item *= 2;
+       cout << item << " ";
+    }
+    cout << endl;
+
+    //Test for front 
+    cout << "## Test for front and back items of theStrings" << endl;
+    cout << theStrings.front() << endl;
+    cout << theStrings.back() << endl;
+
+    // Test for operator []
+    cout << "## Test for operator [2] on theStrings" << endl;
+    cout << theStrings[2] << endl;
+
+    // for const constructor
+    const Blob<string> const_theString(theStrings);
+    auto constItem = const_theString.cbegin();
+    cout << "## Test for const iterator and operator[3] on const_theString" << endl;
+    cout << constItem[3] << endl;
+    
     return 0;
 }
