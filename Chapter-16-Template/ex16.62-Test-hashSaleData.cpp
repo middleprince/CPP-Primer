08bceede9be17942f8379cff0a8644023cdc093b
middleprince <hellangel.ah@gmail.com>
2019-08-06 13:37:20 +0800

    style: Modify output style

--- Chapter-16-Template/ex16.62-Test-hashSaleData.cpp
+++ Chapter-16-Template/ex16.62-Test-hashSaleData.cpp
@@ -10,11 +10,12 @@ using std::unordered_multiset;
 int main() {
     unordered_multiset<SalesData> sale_dataSet;
     sale_dataSet.emplace("More efective C++", 12, 40.7);
-    sale_dataSet.emplace("The C+= programming language", 10, 140.7);
+    sale_dataSet.emplace("The C++ programming language", 10, 140.7);
+    sale_dataSet.emplace("Algorithm", 30, 80.7);
 
     for (const auto &item : sale_dataSet) {
-        cout << "the hash of the book \"%s\"" << item.isbn()
-             << "is :\n0x" << std::hex << hash<SalesData>()(item)
+        cout << "the hash of the book << " << item.isbn() << " >>"
+             << " is :\n0x" << std::hex << hash<SalesData>()(item)
              << endl;
     }
     
