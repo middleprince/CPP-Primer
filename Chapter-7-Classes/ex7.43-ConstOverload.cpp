4522aac959a69ea87985a0b7f8af0dbe45f4ac8d
middleprince <hellangel.ah@gmail.com>
2021-01-07 17:39:53 +0800

    faet: Add string test case

--- Chapter-7-Classes/ex7.43-ConstOverload.cpp
+++ Chapter-7-Classes/ex7.43-ConstOverload.cpp
@@ -5,12 +5,17 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
+
 
 class NoDefault {
 public:
     //NoDefault() = default; 
     NoDefault (const int &num) : mem(num) { }
-    int mem;
+    NoDefault (const std::string &strs) : the_strs(strs) { }
+
+    int mem{1};
+    std::string the_strs{"123"};
 };
 
 struct C {
@@ -21,7 +26,7 @@ struct C {
 
 int main() {
     std::vector<NoDefault> vec(10);
-    std::vector<C> vecC(10);
+    //std::vector<C> vecC(10);
     vec[0].mem = 200;
     std::cout << vec[0].mem << std::endl;
     return 0;
