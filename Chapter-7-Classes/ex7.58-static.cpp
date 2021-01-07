29847c068b1303642ffea007e2bed96990bd0f40
middleprince <hellangel.ah@gmail.com>
2021-01-07 17:58:43 +0800

    doc: minor changes

--- Chapter-7-Classes/ex7.58-static.cpp
+++ Chapter-7-Classes/ex7.58-static.cpp
@@ -5,7 +5,8 @@ class Example {
 public:     
     static constexpr double rate = 6.5;
     //  static member must be initialize out of the class, or 
-    //  define as static constexpr double rate = 6.5;
+    //  define as static constexpr or const double rate = 6.5;
+    //
     static const int vecSize = 20;
     
     static vector<double> vec;
